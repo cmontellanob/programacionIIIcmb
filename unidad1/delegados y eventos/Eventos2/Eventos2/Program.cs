6c37434335c53384f62c1f53afb422555c04eef2
cmontellanob <carlosmontellano@gmail.com>
2020-02-19 07:28:49 +0100

    Tarea 3

--- unidad1/delegados y eventos/Eventos2/Eventos2/Program.cs
+++ unidad1/delegados y eventos/Eventos2/Eventos2/Program.cs
@@ -2,16 +2,31 @@
 
 namespace Eventos2
 {
+    public class Operacionesvector // Esta es la clase publicadora
+    {
+        public delegate void Delegado(int[] arreglo); // lafirma del deleado debe coincidir con la del Manejador de eventod
+        public event Delegado CuandoseIntroduce1; //evento aqui se suscriben lo manejadores de eventos 
+        public  void Llenar(int[] arreglo)
+        {
+            Console.WriteLine("Introduzca valores");
+            for (int i = 0; i < 5; i++)
+            {
+                arreglo[i] = int.Parse(Console.ReadLine());
+                if ((arreglo[i]==-1) && (CuandoseIntroduce1 != null))
+                        {
+                    CuandoseIntroduce1(arreglo);
+                }
+            }
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            int[] arreglo = new int[5]; 
-            Console.WriteLine("Introduzca valores");
-            for (int i=0; i<5;i++)
-            {
-                arreglo[i] = int.Parse(Console.ReadLine());
-            }
+            int[] arreglo = new int[5];
+            Operacionesvector op = new Operacionesvector();
+            op.CuandoseIntroduce1 += MostrarVector; // suscribirse al evento
+            op.Llenar(arreglo);
         }
         static void MostrarVector(int[] arreglo)
         {
