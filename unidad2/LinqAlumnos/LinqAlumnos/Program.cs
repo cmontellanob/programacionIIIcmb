61f7a811cc00a27aec8699356ad8fdb7fbec6856
cmontellanob <carlosmontellano@gmail.com>
2020-03-04 08:31:56 +0100

    Linq Consultas

--- unidad2/LinqAlumnos/LinqAlumnos/Program.cs
+++ unidad2/LinqAlumnos/LinqAlumnos/Program.cs
@@ -13,15 +13,22 @@ namespace LinqAlumnos
                 Alumno[] listaalumnos = new Alumno[8];
                 InsertarTodos(listaalumnos);
                 int opcion = 0;
-                while (opcion != 5)
+                while (opcion != 13)
                 {
                     Console.Clear();
                     Console.WriteLine("1.- Insertar Alumno");
                     Console.WriteLine("2.- Mostrar Alumnos");
                     Console.WriteLine("3.- Mostrar Alumnas");
                     Console.WriteLine("4.- Mostrar Alumnos ordenados alfabeticamente por nombre");
-
-                    Console.WriteLine("5.- Salir");
+                    Console.WriteLine("5.- Mostrar Alumnos mayores de 20");
+                    Console.WriteLine("6.- Mostrar alumnos ordenada por sexo luego por primer apellido descendentemente");
+                    Console.WriteLine("7.- Mostrar Alumnos Agrupadas por Edades");
+                    Console.WriteLine("8.- Cantidad de alumnos por edad");
+                    Console.WriteLine("9.- Promedio de Edad por Sexo");
+                    Console.WriteLine("10.- Las personas de mas edad por sexo");
+                    Console.WriteLine("11.- Los tres almumnos con menor edad");
+                    Console.WriteLine("12.- Los tres almumnos con menor edad despues de la tercera menor edad");
+                    Console.WriteLine("13.- Salir");
                     Console.Write("Introduzca opcion ");
                     opcion = int.Parse(Console.ReadLine());
 
@@ -39,9 +46,33 @@ namespace LinqAlumnos
                         case 4:
                             OrdenarNombre(listaalumnos);
                             break;
+                        case 5:
+                        Mayores20 (listaalumnos);
+                        break;
+                    case 6:
+                        SexoPrimerApellido(listaalumnos);
+                        break;
+                    case 7:
+                        AgruparEdad(listaalumnos);
+                        break;
+                    case 8:
+                        CantidadEdad (listaalumnos);
+                        break;
+                    case 9:
+                        EdadPromediorSexo(listaalumnos);
+                        break;
+                    case 10:
+                        MayoEdadSexo(listaalumnos);
+                        break;
+                    case 11:
+                        TresMenores(listaalumnos);
+                        break;
+                    case 12:
+                        TresMenoresDespues(listaalumnos);
+                        break;
 
 
-                        default:
+                    default:
                             break;
                     }
                 }
@@ -84,11 +115,10 @@ namespace LinqAlumnos
 
             static void Mostrar(Alumno[] listaalumnos)
             {
-                ????    
-              
-                foreach (Alumno a in listaalumnos)
+            var todos = from l in listaalumnos
+                        select l;
+                foreach (Alumno a in todos)
                 {
-                    if (a != null)
                         Console.WriteLine("{0} {1} {2} {3} {4}", a.nombre, a.primerapellido, a.segundoapellido, a.ci, a.edad);
                 }
                 Console.ReadKey();
@@ -96,35 +126,159 @@ namespace LinqAlumnos
             }
             static void MostrarAlumnas(Alumno[] listaalumnos)
             {
-               ?????
-                foreach (Alumno a in listaalumnos)
-                {
-                    if (a != null)
-                        if (a.sexo == "F")
-                            Console.WriteLine("{0} {1} {2} {3} {4}", a.nombre, a.primerapellido, a.segundoapellido, a.ci, a.edad);
-                }
-                Console.ReadKey();
-
+            var alumnas = from l in listaalumnos
+                         where l.sexo=="F"
+                        select l;
+            foreach (Alumno a in alumnas)
+            {
+                Console.WriteLine("{0} {1} {2} {3} {4}", a.nombre, a.primerapellido, a.segundoapellido, a.ci, a.edad);
             }
-            public static void OrdenarNombre(Alumno[] listaalumnos)
+            Console.ReadKey();
+
+        }
+        public static void OrdenarNombre(Alumno[] listaalumnos)
 
             {
 
-             ????
-                for (int j = 0; j < listaalumnos.Length; j++)
-                {
-                    for (int i = 0; i < listaalumnos.Length - 1 - j; i++)
-                    {
-                        if (listaalumnos[i].nombre.CompareTo(listaalumnos[i + 1].nombre) > 0)
-                        {
-                            Alumno aux = listaalumnos[i];
-                            listaalumnos[i] = listaalumnos[i + 1];
-                            listaalumnos[i + 1] = aux;
-                        }
-                    }
-                }
-                Mostrar(listaalumnos);
+            var ordenarnombre = from l in listaalumnos
+                                orderby l.nombre
+                                select l;
+            foreach (Alumno a in ordenarnombre)
+            {
+                Console.WriteLine("{0} {1} {2} {3} {4}", a.nombre, a.primerapellido, a.segundoapellido, a.ci, a.edad);
             }
+            Console.ReadKey();
+        }
+        public static void Mayores20(Alumno[] listaalumnos)
+        {
+
+            var mayores20 = from l in listaalumnos
+                            where l.edad>20
+                            select l;
+            foreach (Alumno a in mayores20)
+            {
+                Console.WriteLine("{0} {1} {2} {3} {4}", a.nombre, a.primerapellido, a.segundoapellido, a.ci, a.edad);
+            }
+            Console.ReadKey();
+        }
+        public static void SexoPrimerApellido(Alumno[] listaalumnos)
+        {
+
+            var SexoApellido = from l in listaalumnos
+                            orderby l.sexo,l.primerapellido descending 
+                            select l;
+            foreach (Alumno a in SexoApellido)
+            {
+                Console.WriteLine("{0} {1} {2} {3} {4} {5}",a.sexo ,a.nombre, a.primerapellido, a.segundoapellido, a.ci, a.edad);
+            }
+            Console.ReadKey();
+        }
+        public static void AgruparEdad(Alumno[] listaalumnos)
+        {
+
+            var Edad = from l in listaalumnos
+                               orderby l.edad descending
+                               group l by l.edad into edad 
+                               select edad;
+            
+            foreach (var GrupoEdades in Edad)
+            {
+                Console.WriteLine(GrupoEdades.Key);
+                foreach (Alumno alumno in GrupoEdades)
+                {
+                    Console.WriteLine("{0} {1}", alumno.nombre, alumno.primerapellido);
+
+                }
+            }
+
+            Console.ReadKey();
+        }
+        public static void CantidadEdad(Alumno[] listaalumnos)
+        {
+
+
+            var cantidad = from l in listaalumnos
+                           orderby l.edad
+                           group l by l.edad into edad
+                           select new { Edad = edad.Key, Cantidad = edad.Count() };
+
+            foreach (var grupoedad in cantidad)
+            {
+                Console.WriteLine("{0} {1}", grupoedad.Edad, grupoedad.Cantidad);
+
+            }
+            Console.ReadKey();
+
+        }
+        public static void EdadPromediorSexo(Alumno[] listaalumnos)
+        {
+
+
+            var edad = from l in listaalumnos
+                           group l by l.sexo into sexo
+                           select new { Sexo = sexo.Key, Promedio = sexo.Average(p=>p.edad) };
+
+            foreach (var grupoedad in edad)
+            {
+                Console.WriteLine("{0} {1}", grupoedad.Sexo, grupoedad.Promedio);
+
+            }
+            Console.ReadKey();
+
+        }
+        public static void MayoEdadSexo(Alumno[] listaalumnos)
+        {
+
+
+            var mayor = from l in listaalumnos
+                       group l by l.sexo into sexo
+                       select new { Sexo = sexo.Key, Edad = sexo.Max(p => p.edad) };
+
+            foreach (var grupoedad in mayor)
+            {
+                Console.WriteLine("{0} {1}", grupoedad.Sexo, grupoedad.Edad);
+
+            }
+            Console.ReadKey();
+
+        }
+        public static void TresMenores(Alumno[] listaalumnos)
+        {
+
+
+            var menores = (from l in listaalumnos
+                          orderby l.edad
+                          
+                          select l).Take(3);
+
+
+            foreach (Alumno a in menores)
+            {
+                Console.WriteLine("{0} {1} {2} {3} {4} {5}", a.sexo, a.nombre, a.primerapellido, a.segundoapellido, a.ci, a.edad);
+            }
+            
+            Console.ReadKey();
+
+        }
+        public static void TresMenoresDespues(Alumno[] listaalumnos)
+        {
+
+
+            var menores = (from l in listaalumnos
+                           orderby l.edad
+
+                           select l).Skip(3).Take(3);
+
+
+            foreach (Alumno a in menores)
+            {
+                Console.WriteLine("{0} {1} {2} {3} {4} {5}", a.sexo, a.nombre, a.primerapellido, a.segundoapellido, a.ci, a.edad);
+            }
+
+            Console.ReadKey();
+
         }
     }
+
 }
+
