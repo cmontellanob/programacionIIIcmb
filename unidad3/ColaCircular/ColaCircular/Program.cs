9b7c4a9273b309833bfb37611fe4fb31f24661c9
cmontellanob <carlosmontellano@gmail.com>
2020-03-18 08:18:35 -0400

    18/03/20

---
+++ unidad3/ColaCircular/ColaCircular/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ColaCircular
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcion = 0;
+            int x = 0;  
+            ColaCircular<int> p = new ColaCircular<int>();
+            while (opcion != 4)
+            {
+                Console.Clear();
+                Console.WriteLine("1. Insertar");
+                Console.WriteLine("2. Eliminar");
+                Console.WriteLine("3. Mostrar");
+                Console.WriteLine("4. Salir");
+
+                opcion = int.Parse(Console.ReadLine());
+                switch (opcion)
+                {
+                    case 1:
+                        Console.WriteLine("introduzca un elemento");
+                        x = int.Parse(Console.ReadLine());
+                        p.Insertar(x);
+                        break;
+                    case 2:
+                        x = p.Eliminar();
+                        Console.WriteLine("El elemento eliminado es {0}", x);
+                        Console.ReadKey();
+                        break;
+                    case 3:
+                        p.mostrar();
+                        Console.ReadKey();
+                        break;
+
+                }
+
+            }
+
+        }
+    }
+}
